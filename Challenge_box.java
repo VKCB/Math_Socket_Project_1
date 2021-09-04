42350bc0856d6a3bdc6e9d7d7f419f0ca12a2c57
Alejadra <alerodriguezc2002@gmail.com>
2021-09-05 00:42:37 +0100

    Creacion del metodo que genera signo random

--- Challenge_box.java
+++ Challenge_box.java
@@ -1,6 +1,8 @@
 
 package My_Challenge_box.Math_Socket_Project_1;
 
+import java.util.Random;
+
 public class Challenge_box {
     private static int x,y;
     private static float resp;
@@ -9,10 +11,18 @@ public class Challenge_box {
     public static void main(String[] args) {
         
     }
-    }
     
     static int generate_random(){
         int max=50,min=0;
         int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
         return random_int;
+    }
+    
+    static char generate_random_c(){
+        Random random = new Random();
+        String setOfChar = "+-*/";
+        int randomInt = random.nextInt(setOfChar.length());
+        char randomChar = setOfChar.charAt(randomInt);
+        return randomChar;
+    }
 }
