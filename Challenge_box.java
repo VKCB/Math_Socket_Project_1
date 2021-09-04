93b51ce06da79c590aa694833f617e6d880bcf64
Alejadra <alerodriguezc2002@gmail.com>
2021-09-05 00:40:56 +0100

    Creacion del metodo que genera numeros random

---
+++ Challenge_box.java
@@ -0,0 +1,18 @@
+
+package My_Challenge_box.Math_Socket_Project_1;
+
+public class Challenge_box {
+    private static int x,y;
+    private static float resp;
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    }
+    
+    static int generate_random(){
+        int max=50,min=0;
+        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+        return random_int;
+}
