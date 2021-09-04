fa759d595af60cd831eb685f33d97e63591e7cdd
Alejadra <alerodriguezc2002@gmail.com>
2021-09-05 00:46:44 +0100

    Creacion del metodo que verifica la respuesta

--- Challenge_box.java
+++ Challenge_box.java
@@ -2,6 +2,7 @@
 package My_Challenge_box.Math_Socket_Project_1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Challenge_box {
     private static int x,y;
@@ -24,5 +25,38 @@ public class Challenge_box {
         int randomInt = random.nextInt(setOfChar.length());
         char randomChar = setOfChar.charAt(randomInt);
         return randomChar;
+    }
+    
+    public static void generate_problem(){
+        x=Challenge_box.generate_random();
+        y=Challenge_box.generate_random();
+        char simb = Challenge_box.generate_random_c();
+        if(simb == '+'){
+            System.out.println(x+" + "+y+" = ?");
+            resp = x+y;
+        }
+        if(simb == '-'){
+            System.out.println(x+" - "+y+" = ?");
+            resp = x-y;
+        }
+        if(simb == '*'){
+            System.out.println(x+" x "+y+" = ?");
+            resp = x*y;
+        }
+        if(simb == '/'){
+            System.out.println(x+" / "+y+" = ?");
+            resp = x/y;
+        }
+    }
+    
+    public static void check_answer(){
+        Scanner ans = new Scanner(System.in);
+        System.out.println("Digite la respuestas del problema:");
+        float user_ans = ans.nextFloat();
+        if (user_ans == resp){
+            System.out.println("Respuesta correcta");
+        }else{
+            System.out.println("Respuesta incorrecta, el resultado es: "+resp);
+        }
     }
 }
