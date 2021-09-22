0c65787259b83c46d8332e837af631f9ca20786a
VKCB <kbran1409@gmail.com>
2021-09-22 01:13:44 +0100

    Se crea clase cliente

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.pkg1.Math_Socket_Project_1;
+
+/**
+ *
+ * @author Valerine
+ */
+public class Cliente {
+    
+    public static void main(String[] args) {}
+ 
